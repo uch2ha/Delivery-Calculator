00592e91acea2214b7857bd77533935a55d88292
uch2ha <sinyavskiy.d@gmail.com>
2023-01-31 19:36:53 +0200

    added about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import MainPage from './components/mainPage/MainPage';
 import HistoryPage from './components/historyPage/HistoryPage';
+import AboutPage from './components/about/AboutPage';
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
       </Routes>
       <Routes>
         <Route path='/history' element={<HistoryPage />} />
+      </Routes>
+      <Routes>
+        <Route path='/about' element={<AboutPage />} />
       </Routes>
     </BrowserRouter>
   );

f23e9ed85f51339dbc9d916c6c7250f8eb9abe5e
uch2ha <sinyavskiy.d@gmail.com>
2023-01-31 19:30:56 +0200

    new interface data type

--- src/interfaces/DeliveryFeeData.interface.ts
+++ src/interfaces/DeliveryFeeData.interface.ts
@@ -1,7 +1,9 @@
+import moment from 'moment-timezone';
+
 export interface IdeliveryFeeData {
   cartValue: number;
   distance: number;
   itemsAmount: number;
-  date: string;
+  date: moment.Moment;
   delivery_price?: number;
 }
